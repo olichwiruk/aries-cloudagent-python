c7a970da08f468f757e4b2973ede08cb6b0fa738
Marcin <marcin@opensoftware.pl>
2020-08-27 16:24:49 +0200

    Handle spaces in AGENT_NAME variable

--- startup.sh
+++ startup.sh
@@ -2,14 +2,14 @@ aca-py start \
     -it http 0.0.0.0 80 \
     -ot http \
     -e $ACAPY_ENDPOINT \
-    --label $AGENT_NAME \
+    --label "$AGENT_NAME" \
     --auto-accept-requests --auto-ping-connection \
     --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
     --auto-respond-presentation-proposal --auto-respond-presentation-request --auto-verify-presentation \
-    --invite --invite-role admin --invite-label "$AGENT_NAME (admin)" \
+    --invite --invite-role admin --invite-label "$AGENT_NAME" \
     --admin 0.0.0.0 $SWAGGER_INBOUND_PORT --admin-insecure-mode \
     --genesis-url $GENESIS_URL \
     --wallet-type indy \
-    --wallet-name $AGENT_NAME \
+    --wallet-name "$AGENT_NAME" \
     --plugin acapy_plugin_toolbox \
     --plugin services
