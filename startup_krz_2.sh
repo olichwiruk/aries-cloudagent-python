ae9dee33e60391be77c83943614a24b6c1986cd6
Karol Krzosa <krzosa.karol@gmail.com>
2020-10-08 12:14:37 +0200

    PullRequest

--- startup_krz_2.sh
+++ startup_krz_2.sh
@@ -3,15 +3,15 @@ aca-py start \
     -ot http \
     -ot ws \
     -e $ACAPY_ENDPOINT \
-    --label "$AGENT_NAME" \
+    --label $AGENT_NAME \
     --auto-accept-requests --auto-ping-connection \
     --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
     --auto-respond-presentation-proposal --auto-respond-presentation-request --auto-verify-presentation \
-    --invite --invite-role admin --invite-label "$AGENT_NAME" \
-    --admin 0.0.0.0 $SWAGGER_INBOUND_PORT --admin-insecure-mode \
-    --webhook-url $WEBHOOK_URL \
+    --invite --invite-role admin --invite-label "$AGENT_NAME (admin)" \
     --genesis-url $GENESIS_URL \
     --wallet-type indy \
-    --wallet-name "$AGENT_NAME" \
+    --wallet-name $AGENT_NAME \
+    --admin 0.0.0.0 8150 --admin-insecure-mode \
+    --webhook-url ws://websocket_server$1:5000 \
     --plugin acapy_plugin_toolbox \
-    --plugin services
+    --plugin services 
