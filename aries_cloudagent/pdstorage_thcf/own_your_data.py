ebe9a0ad27be6f67c3b422629e7cdb1d39f18108
Marcin <marcin@opensoftware.pl>
2020-11-19 16:20:47 +0100

    Update OYD pds plugin api and make API host url configurable

--- aries_cloudagent/pdstorage_thcf/own_your_data.py
+++ aries_cloudagent/pdstorage_thcf/own_your_data.py
@@ -4,24 +4,21 @@ from .error import *
 
 import json
 import logging
+from urllib.parse import urlparse
 
 from aiohttp import ClientSession, FormData
 
 LOGGER = logging.getLogger(__name__)
 
-API_DATA_VAULT = "https://data-vault.eu"
-API_TOKEN = API_DATA_VAULT + "/oauth/token"
-API_ON_SAVE = API_DATA_VAULT + "/api/repos/dip.data/items"
-API_ON_READ = API_DATA_VAULT + "/api/items"
-
 
 class OwnYourDataVault(BasePersonalDataStorage):
     def __init__(self):
         super().__init__()
+        self.api_url = None
         self.token = None
         self.preview_settings = {
             "oca_schema_namespace": "pds",
-            "oca_schema_dri": "4ZsViHFpyTYFdrrpRQmgun1qG4WKaC9rEXU3BpT7Foq2",
+            "oca_schema_dri": "cB9LRYioVa4VDcgi76cbEXv9Y53W7CuuLWrFwg7cXnT9",
         }
 
     async def update_token(self):
@@ -29,9 +26,15 @@ class OwnYourDataVault(BasePersonalDataStorage):
         # if self.token != None:
         #     return
 
+        parsed_url = urlparse(self.settings.get("api_url"))
+        self.api_url = '{url.scheme}://{url.netloc}'.format(url=parsed_url)
         client_id = self.settings.get("client_id")
         client_secret = self.settings.get("client_secret")
 
+        if self.api_url is None:
+            raise PersonalDataStorageLackingConfigurationError(
+                "Please configure the plugin, api_url is empty"
+            )
         if client_id == None:
             raise PersonalDataStorageLackingConfigurationError(
                 "Please configure the plugin, Client_id is empty"
@@ -43,7 +46,7 @@ class OwnYourDataVault(BasePersonalDataStorage):
 
         async with ClientSession() as session:
             result = await session.post(
-                API_TOKEN,
+                self.api_url + "/oauth/token",
                 json={
                     "client_id": client_id,
                     "client_secret": client_secret,
@@ -60,23 +63,22 @@ class OwnYourDataVault(BasePersonalDataStorage):
             self.token = token
             LOGGER.info("update token: %s", self.token)
 
-    async def load(self, id: str) -> str:
+    async def load(self, dri: str) -> str:
         """
         TODO: Errors checking
         """
         await self.update_token()
 
-        url = f"https://data-vault.eu/api/data?dri={id}"
-        # url = f"https://data-vault.eu/api/data?dri={dri_value}&schema_dri={dri_schema_value}"
+        url = f"{self.api_url}/api/data/{dri}?p=dri&f=plain"
         async with ClientSession() as session:
             result = await session.get(
                 url, headers={"Authorization": "Bearer " + self.token["access_token"]}
             )
-            result = await result.text()
-            result = json.loads(result)
-            LOGGER.info("Result of GET request %s", result)
+            result_str = await result.text()
+            result_dict = json.loads(result_str)
+            LOGGER.info("Result of GET request %s", result_str)
 
-        return result.get("content")
+        return result_str
 
     async def save(self, record: str) -> str:
         oyd_repo = self.settings.get("repo")
@@ -84,13 +86,13 @@ class OwnYourDataVault(BasePersonalDataStorage):
         await self.update_token()
         async with ClientSession() as session:
             result = await session.post(
-                "https://data-vault.eu/api/data",
+                f"{self.api_url}/api/data",
                 headers={"Authorization": "Bearer " + self.token["access_token"]},
                 json={
-                    "content": record,
+                    "content": json.loads(record),
                     "dri": dri_value,
                     # "schema_dri": dri_schema_value,
-                    "mime_type": "application/json",
+                    # "mime_type": "application/json",
                     "table_name": oyd_repo if oyd_repo != None else "dip.data",
                 },
             )
